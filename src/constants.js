f381c3b056217fd8eb682f8fa8bce2f0d0e837f2
rheiderabe <rheiderline@gmail.com>
2022-01-28 19:34:11 -0300

    gg project nft

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+const CONTRACT_ADDRESS = "0x7f1b84c6238D56d16466d1155D6b7073212b1675";
+
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData };
